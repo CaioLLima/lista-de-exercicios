aca1b285526542df6f3c5b8ba9e47bd3805e9d78
CaioLLima <caiololima2@gmail.com>
2025-04-03 20:57:28 -0300

    Conclusão exercício 08

--- ListaDeExercicios.Exercicio07/Program.cs
+++ ListaDeExercicios.Exercicio07/Program.cs
@@ -4,35 +4,25 @@
     {
         static void Main(string[] args)
         {
-            //Crie um programa para calcular a média ponderada de duas provas realizadas por um aluno
+            //Crie um programa para verificar se um número é primo.
             #region input de dados
-            Console.Write("Informe o peso da primeira prova: ");
-            double pesoProva1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Informe a nota da primeira prova: ");
-            double notaProva1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Informe o peso da segunda prova: ");
-            double pesoProva2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Informe a nota da segunda prova: ");
-            double notaProva2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Informe o peso da segunda prova: ");
-            double pesoProva3 = Convert.ToDouble(Console.ReadLine());
-
-            Console.Write("Informe a nota da segunda prova: ");
-            double notaProva3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Insira um número para verificar se é primo: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
             #endregion
 
             #region processamento
-            double mediaPonderada = (pesoProva1 * notaProva1) + (pesoProva2 * notaProva2) + (pesoProva3 * notaProva3);
-            double divisao = pesoProva1 + notaProva2 + notaProva3;
+            if (numero > 0 && )
+            {
+
+            }
+            
             #endregion
 
             #region output dos dados
-            Console.Write($"A media ponderada das duas provas é de : {mediaPonderada / divisao}");
+            Console.Write($"o número {numero} é um número primo.");
             #endregion
+
+            Console.ReadLine();
         }
     }
 }
--- ListaDeExercicios.Exercicio08/Program.cs
+++ ListaDeExercicios.Exercicio08/Program.cs
@@ -6,19 +6,26 @@
         {
             //Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
             #region input de dados
-            Console.Write("Digite a temperatura em Celsius: ");
-            double tempCelsius = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Insira o primeiro valor(A): ");
+            double valor1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Insira o segundo valor(B): ");
+            double valor2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Insira o terceiro valor(C): ");
+            double valor3 = Convert.ToDouble(Console.ReadLine());
             #endregion
 
             #region processamento
-            double tempKelvin = tempCelsius * 1.8 + 32;
-            #endregion
+            if (valor1 + valor2 < valor3)
+                Console.Write($"A soma de A={valor1} + B={valor2} = {valor1 + valor2} é menor que o valor C={valor3}.");
+            else if (valor1 + valor2 > valor3)
+                Console.WriteLine($"A soma de A + B={valor2} = {valor1 + valor2} é maior que o valor C={valor3}.");
+            else
+                Console.WriteLine("Valores inválidos");
+                #endregion
 
-            #region output dos dados
-            Console.Write($"A temperatura em Fahrenheint é de: {tempKelvin}");
-            #endregion
-
-            Console.ReadLine();
+                Console.ReadLine();
         }
     }
 }
