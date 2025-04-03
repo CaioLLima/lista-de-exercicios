496907612bf7294f6cdede16220c02ba9e6b601c
CaioLLima <caiololima2@gmail.com>
2025-04-03 19:59:18 -0300

    Exercício 04

---
+++ ListaDeExercicios.Exercicio04/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ListaDeExercicios.Exercicio04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+
+            #region input de dados
+            Console.Write("Digite a temperatura em Celsius: ");
+            double tempCelsius = Convert.ToDouble(Console.ReadLine());
+            #endregion
+
+            #region processamento
+            double tempKelvin = tempCelsius * 1.8 + 32;
+            #endregion
+
+            #region output dos dados
+            Console.Write($"A temperatura em Fahrenheint é de: {tempKelvin}");
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
