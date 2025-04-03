961ebdc467e93cb70c2cff65f3fd5e841cbe4a05
CaioLLima <caiololima2@gmail.com>
2025-04-03 20:02:50 -0300

    Criação todos projetos, códigos vazios

---
+++ ListaDeExercicios.Exercicio11/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ListaDeExercicios.Exercicio11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ ListaDeExercicios.Exercicio12/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ListaDeExercicios.Exercicio12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
