e0d9644bb0d43dd513fef659943560502bab56e2
CaioLLima <caiololima2@gmail.com>
2025-04-03 19:42:58 -0300

    Add project files.

---
+++ ListaDeExercicios.Exercicio01/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ListaDeExercicios.Exercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crie um programa para calcular o volume de uma caixa retangular
+            //VOLUME_DA_CAIXA Comprimento * Largura * Altura
+
+            #region input de dados
+            Console.Write("Digite o comprimento da caixa: ");
+            decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Digite a largura da caixa: ");
+            decimal largura = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Digite a altura da caixa: ");
+            decimal altura = Convert.ToDecimal(Console.ReadLine());
+            #endregion
+
+            #region processamento
+            decimal volume_caixa = comprimento * largura * altura;
+            #endregion
+
+            #region output dos dados
+            Console.Write($"O volume da caixa é de: {volume_caixa}.");
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ListaDeExercicios.Exercicio02/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ListaDeExercicios.Exercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crie um programa para calcular o volume de um cilíndro
+            //VOLUME_DA_CILINDRO V = pi * raio² * a
+
+            #region input de dados
+
+            Console.Write("Digite o raio do cilindro: ");
+            double raio = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite a altura do cilindro: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            #endregion
+
+            #region processamento
+            double resultado = Math.PI * Math.Pow(raio, 2) * altura;
+            #endregion
+
+            #region output dos dados
+            Console.Write($"O tamanho da caida é de: {resultado} ");
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
