4905add37cfd42d87fd6fe16e0fd333102263902
CaioLLima <caiololima2@gmail.com>
2025-04-04 12:37:43 -0300

    Conclusão exercício 11, aprendendo expressão ternária

--- ListaDeExercicios.Exercicio11/Program.cs
+++ ListaDeExercicios.Exercicio11/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ListaDeExercicios.Exercicio11
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ListaDeExercicios.Exercicio11
 {
     internal class Program
     {
@@ -7,16 +9,20 @@
             //Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.
 
             #region input de dados
-            Console.Write("Digite a temperatura em Celsius: ");
-            double tempCelsius = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Insira um número para verifica se é par ou ímpar: ");
+            double parOuImpar = Convert.ToDouble(Console.ReadLine());
             #endregion
 
             #region processamento
-            double tempKelvin = tempCelsius * 1.8 + 32;
+            /*if (parOuImpar % 2 == 0)
+            {
+                Console.WriteLine($"O número {parOuImpar} é par.");
+            } else
+                Console.WriteLine($"O número {parOuImpar} é ímpar.");*/
             #endregion
 
-            #region output dos dados
-            Console.Write($"A temperatura em Fahrenheint é de: {tempKelvin}");
+            #region output dos dados 
+            Console.WriteLine(parOuImpar % 2 == 0 ? ($"O número {parOuImpar} é par.") : ($"O número {parOuImpar} é ímpar.")); //expressão ternária
             #endregion
 
             Console.ReadLine();
