572206ae006679c452978b7355df2333cf0613d7
CaioLLima <caiololima2@gmail.com>
2025-04-03 21:18:03 -0300

    Conclusão exercício 09

--- ListaDeExercicios.Exercicio09/Program.cs
+++ ListaDeExercicios.Exercicio09/Program.cs
@@ -6,16 +6,49 @@
         {
             //Escreva um algoritmo que leia três valores inteiros e diferentes e mostre-os em ordem decrescente.
             #region input de dados
-            Console.Write("Digite a temperatura em Celsius: ");
-            double tempCelsius = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Insira o primeiro valor: ");
+            double valor1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Insira o segundo valor: ");
+            double valor2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Insira o terceiro valor: ");
+            double valor3 = Convert.ToDouble(Console.ReadLine());
             #endregion
+
+            double[] ordemValores = [valor1,valor2,valor3];
 
             #region processamento
-            double tempKelvin = tempCelsius * 1.8 + 32;
+            /* Maneira Manual complicada
+             
+             if (valor1 > valor2 && valor1 > valor3)       
+                maiorValor = valor1;
+            else if (valor2 > valor1 && valor2 > valor3)
+                maiorValor = valor2;
+            else
+                maiorValor = valor3;
+
+            if (valor1 < valor2 && valor1 < valor3)
+                menorValor = valor1;
+            else if (valor2 < valor1 && valor2 < valor3)
+                menorValor = valor2;
+            else
+                menorValor = valor3;
+
+
+            if (valor1 != maiorValor && valor1 != menorValor)
+                medioValor = valor1;
+            else if (valor2 != maiorValor && valor2 != menorValor)
+                medioValor = valor2;
+            else
+                medioValor = valor3;*/
+
+            Array.Sort(ordemValores);
+            Array.Reverse(ordemValores);
             #endregion
 
-            #region output dos dados
-            Console.Write($"A temperatura em Fahrenheint é de: {tempKelvin}");
+                #region output dos dados
+                Console.Write($"Os valores em ordem decrescente é: {ordemValores[0]}, {ordemValores[1]}, {ordemValores[2]}");
             #endregion
 
             Console.ReadLine();
