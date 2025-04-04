dff38d1a8f8842057189481b4c8f5d209a5f9cd2
CaioLLima <caiololima2@gmail.com>
2025-04-04 13:18:15 -0300

    Conclusão exercício 13

--- ListaDeExercicios.Exercicio13/Program.cs
+++ ListaDeExercicios.Exercicio13/Program.cs
@@ -1,20 +1,44 @@
-﻿namespace ListaDeExercicios.Exercicio13
+﻿using System.Linq.Expressions;
+
+namespace ListaDeExercicios.Exercicio13
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            /*Escreva um algoritmo que leia um valor inicial A e imprima a sequência de valores do cálculo de A! e o seu
+            resultado.
+                a.Ex: 5! = 5 X 4 X 3 X 2 X 1 = 120
+                b.Pesquise sobre “fatorial”*/
             #region input de dados
-            Console.Write("Digite a temperatura em Celsius: ");
-            double tempCelsius = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Insira o número para verificar seu fatorial: ");
+            string entrada = Console.ReadLine();
             #endregion
 
             #region processamento
-            double tempKelvin = tempCelsius * 1.8 + 32;
+            if (int.TryParse(entrada, out int numero) && numero >= 0)
+            {
+                int fatorial = 1;
+                string expressao = "";
+                for (int i = numero; i >= 1; i--)
+                {
+                    fatorial *= i;
+                    expressao += i;
+
+                    if (i > 1)
+                        expressao += " x ";
+                }
+
+                Console.WriteLine($"{numero}! = {expressao} = {fatorial}");
+            }
+            else
+            {
+                Console.WriteLine("Digite um número inteiro válido e não-negativo.");
+            }
             #endregion
 
             #region output dos dados
-            Console.Write($"A temperatura em Fahrenheint é de: {tempKelvin}");
+            
             #endregion
 
             Console.ReadLine();
