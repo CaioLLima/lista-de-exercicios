aafb0feba7f6a9691ea047a08f4b5aa33481ca76
CaioLLima <caiololima2@gmail.com>
2025-04-07 13:16:46 -0300

    Conclusão exercício 15

--- ListaDeExercicios.Exercicio15/Program.cs
+++ ListaDeExercicios.Exercicio15/Program.cs
@@ -4,17 +4,36 @@
     {
         static void Main(string[] args)
         {
+            /*  Escreva um programa que imprima os números de 1 a 100 em ordem crescente, substituindo os números
+                múltiplos de 3 pela palavra "Fizz" e os múltiplos de 5 pela palavra "Buzz". Para números que são múltiplos de
+                ambos, use "FizzBuzz".*/
+
             #region input de dados
-            Console.Write("Digite a temperatura em Celsius: ");
-            double tempCelsius = Convert.ToDouble(Console.ReadLine());
+
             #endregion
 
             #region processamento
-            double tempKelvin = tempCelsius * 1.8 + 32;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write($"FizzBuzz ");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.Write($"Fizz ");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write($"Buzz ");
+                }
+
+                Console.Write($"{i} ");
+            }
             #endregion
 
             #region output dos dados
-            Console.Write($"A temperatura em Fahrenheint é de: {tempKelvin}");
+           
             #endregion
 
             Console.ReadLine();
